fe485693ffa7f0d0ba44184fc65ad12857b3b37f
sicktooth <sosaronungwe@gmail.com>
2023-06-17 09:56:44 +0100

    added the files and index

---
+++ Resources/js/script.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+    
+    /* Sticky Navigation */
+    $('.js--section-features').waypoint(function(direction){
+        if(direction == "down") {
+            $('nav').addClass('sticky');
+        } 
+        else { 
+            $('nav').removeClass('sticky');
+             }
+    }, {
+            offset: '60px;'
+            });
+    
+    
+     /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+    
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1500);
+            return false;
+          }
+        }
+      });
+    });
+    
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    },
+        {
+        offset:'50%'
+    });
+    
+     $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animate__animated animate__slideInUp');
+    },
+        {
+        offset:'50%'
+    });
+    
+     $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animate__animated animate__bounceInLeft');
+    },
+        {
+        offset:'50%'
+    });
+    
+     $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animate__animated animate__heartBeat');
+    },
+        {
+        offset:'50%'
+    });
+    
+    
+    /* Mobile Nav */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('icon ion-ios-menu')) {
+            icon.addClass('ion-md-close');
+            icon.removeClass('ion-ios-menu');
+        }
+        else {
+            icon.addClass('ion-ios-menu');
+            icon.removeClass('ion-md-close');
+        }
+        
+    });
+    
+    
+});
